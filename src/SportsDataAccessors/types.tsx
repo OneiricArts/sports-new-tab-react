ae8e29208b551c07341d326284c57d350b12cd2c
Dilraj Singh <dilrajsingh.7@gmail.com>
2019-05-12 19:27:41 -0700

    copy simple NFL render

---
+++ src/SportsDataAccessors/types.tsx
@@ -0,0 +1,23 @@
+export interface Game {
+  id: number;
+
+  status: string;
+  awayTeam: string;
+  homeTeam: string;
+  awayTeamWinning: boolean;
+  homeTeamWinning: boolean;
+  awayTeamScore: number;
+  homeTeamScore: number;
+
+  hidden: boolean;
+}
+
+export interface Schedule {
+  displayDate: string;
+  //  playoffs?: boolean;
+  games: Game[];
+}
+
+// export interface NFLGame extends Game {
+//   teamHasPossession?: (team: string) => boolean;
+// }
