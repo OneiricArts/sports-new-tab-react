747dcff749932f17f5f5ca1a66bb9e2b81fcaa97
Dilraj Singh <dilrajsingh.7@gmail.com>
2020-09-27 11:45:44 -0700

    fetch ESPN NFL data for season week and game status

---
+++ src/SportsDataAccessors/nfl/getEspnNflData.ts
@@ -0,0 +1,60 @@
+import { NFLGame } from '../types';
+import { EspnNfl, EventsEntity } from './EspnNflTypes';
+
+type EspnGame = Partial<NFLGame>;
+
+export type EsnpSchedule = {
+  displayDate?: string;
+  games?: EspnGame[];
+};
+
+const getEspnNflData = async (): Promise<EsnpSchedule | undefined> => {
+  const url = `https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard`;
+
+  try {
+    const response = await fetch(url);
+    const data: EspnNfl = await response.json();
+
+    return labelData(data);
+  } catch {
+    return undefined;
+  }
+};
+
+const labelData = (data: EspnNfl): EsnpSchedule => {
+  const games = data.events?.map(g => labelGame(g));
+
+  const seasonName = data.leagues[0].season.type.name;
+  const weekNumber = data.week.number;
+
+  const displayDate = `${seasonName} week ${weekNumber}`;
+
+  return { displayDate, games };
+};
+
+const labelGame = (game: EventsEntity): EspnGame => {
+  const homeCompetitor = game?.competitions?.[0]?.competitors?.find(
+    e => e.homeAway === 'home'
+  );
+
+  const awayCompetitor = game?.competitions?.[0]?.competitors?.find(
+    e => e.homeAway === 'away'
+  );
+
+  let homeTeam = homeCompetitor?.team.name || '';
+  let awayTeam = awayCompetitor?.team.name || '';
+
+  if (homeCompetitor?.team.location.toLocaleLowerCase() === 'washington')
+    homeTeam = 'Football Team';
+
+  if (awayCompetitor?.team.location.toLocaleLowerCase() === 'washington')
+    awayTeam = 'Football Team';
+
+  return {
+    status: { type: 'TIME_STRING', value: game.date },
+    homeTeam,
+    awayTeam
+  };
+};
+
+export default getEspnNflData;
