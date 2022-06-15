95572c40a16e683b38bb4f8a0993252811aa563a
Dilraj Singh <dilrajsingh.7@gmail.com>
2022-06-14 17:37:22 -0700

    fix prettier

--- src/components/NBAExpandedContent.tsx
+++ src/components/NBAExpandedContent.tsx
@@ -1,50 +1,53 @@
 import React from 'react';
 import { ExpandedContentWrapper } from './GameTable';
 
-export const getNbaExpandedContent = (opts: {
-  broadcaster?: string;
-  awayTeam?: string;
-  homeTeam?: string;
-  awayTeamRecord?: string;
-  homeTeamRecord?: string;
-  awayTeamRank?: string;
-  homeTeamRank?: string;
-}) => () => {
-  if (Object.values(opts).every(v => v === undefined)) return undefined;
-  return (
-    <ExpandedContentWrapper>
-      <div style={{ display: 'flex' }}>
-        {[
-          {
-            teamName: opts.awayTeam,
-            record: opts.awayTeamRecord,
-            rank: opts.awayTeamRank
-          },
-          {
-            teamName: opts.homeTeam,
-            record: opts.homeTeamRecord,
-            rank: opts.homeTeamRank
-          }
-        ].map((t, i) => (
-          <div
-            style={{
-              flex: '1',
-              textAlign: i === 0 ? 'right' : 'left',
-              borderRight: i === 0 ? '1px solid rgb(128, 128, 128)' : undefined,
-              padding: '10px'
-            }}
-            key={`${t.teamName}`}
-          >
-            {t.teamName && <h6>{t.teamName}</h6>}
-            {t.rank && <div>#{t.rank}</div>}
-            {t.record && <div>{t.record}</div>}
-          </div>
-        ))}
-      </div>
+export const getNbaExpandedContent =
+  (opts: {
+    broadcaster?: string;
+    awayTeam?: string;
+    homeTeam?: string;
+    awayTeamRecord?: string;
+    homeTeamRecord?: string;
+    awayTeamRank?: string;
+    homeTeamRank?: string;
+  }) =>
+  () => {
+    if (Object.values(opts).every(v => v === undefined)) return undefined;
+    return (
+      <ExpandedContentWrapper>
+        <div style={{ display: 'flex' }}>
+          {[
+            {
+              teamName: opts.awayTeam,
+              record: opts.awayTeamRecord,
+              rank: opts.awayTeamRank
+            },
+            {
+              teamName: opts.homeTeam,
+              record: opts.homeTeamRecord,
+              rank: opts.homeTeamRank
+            }
+          ].map((t, i) => (
+            <div
+              style={{
+                flex: '1',
+                textAlign: i === 0 ? 'right' : 'left',
+                borderRight:
+                  i === 0 ? '1px solid rgb(128, 128, 128)' : undefined,
+                padding: '10px'
+              }}
+              key={`${t.teamName}`}
+            >
+              {t.teamName && <h6>{t.teamName}</h6>}
+              {t.rank && <div>#{t.rank}</div>}
+              {t.record && <div>{t.record}</div>}
+            </div>
+          ))}
+        </div>
 
-      {opts.broadcaster && (
-        <div style={{ textAlign: 'center' }}>{`📺 ${opts.broadcaster}`}</div>
-      )}
-    </ExpandedContentWrapper>
-  );
-};
+        {opts.broadcaster && (
+          <div style={{ textAlign: 'center' }}>{`📺 ${opts.broadcaster}`}</div>
+        )}
+      </ExpandedContentWrapper>
+    );
+  };
