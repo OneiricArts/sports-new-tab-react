0de926daae69f81da41d2a7202a1dca153b53cc2
Dilraj Singh <dilrajsingh.7@gmail.com>
2024-05-21 02:00:30 -0700

    Use updated styles in Link card

--- src/components/Links/AddSiteBar.tsx
+++ src/components/Links/AddSiteBar.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState } from 'react';
-import { Button, Form, Input } from 'reactstrap';
+import { Form } from 'reactstrap';
 import { LinkI } from './Links';
+import { Button } from '../UI/Button';
 
 type AddSiteProps = {
   addSite: (site: LinkI) => void;
@@ -11,33 +12,22 @@ export const AddSiteBar: FC<AddSiteProps> = ({ addSite }) => {
   const [url, setUrl] = useState('');
 
   return (
-    <Form
-      inline
-      className="py-2 px-3 d-flex border-bottom"
-      style={{ gap: '5px' }}
-    >
-      <Input
+    <Form inline className="py-2  d-flex" style={{ gap: '5px' }}>
+      <input
         placeholder="Name"
+        style={{ flexGrow: 0 }}
         value={title}
         onChange={e => setTitle(e.target.value)}
       />
 
-      <Input
+      <input
         placeholder="Url - including http(s)"
         className="flex-grow-1"
         value={url}
         onChange={e => setUrl(e.target.value)}
       />
 
-      <Button
-        color="primary"
-        outline
-        size="md"
-        className="ml-auto"
-        onClick={() => addSite({ title, url })}
-      >
-        Add
-      </Button>
+      <Button onClick={() => addSite({ title, url })}>Add</Button>
     </Form>
   );
 };
